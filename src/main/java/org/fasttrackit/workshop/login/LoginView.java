43905ac2d2fee553edf253ad5a757688668c294a
ioanaselegean <ioana.selegean@ebsromania.ro>
2015-07-04 13:39:17 +0300

    Refactor login page - LoginView and Testy

---
+++ src/main/java/org/fasttrackit/workshop/login/LoginView.java
@@ -0,0 +1,39 @@
+package org.fasttrackit.workshop.login;
+
+import com.sdl.selenium.web.WebLocator;
+import com.sdl.selenium.web.button.Button;
+import com.sdl.selenium.web.form.TextField;
+import com.sdl.selenium.web.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class LoginView {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginView.class);
+
+    private Button loginButtonEl = new Button().setId("loginButton");
+
+    private TextField emailEl = new TextField().setId("email");
+
+    private TextField passEl = new TextField().setId("password");
+
+    private WebLocator errorEl = new WebLocator().setClasses("error-msg");
+
+    public void clickOnLoginButton() {
+        loginButtonEl.click();
+    }
+
+    public void enterCredentials(String emailVal, String passVal) {
+        emailEl.sendKeys(emailVal);
+        System.out.println("Enter email:" + emailVal);
+        passEl.sendKeys(passVal);
+        System.out.println("Enter password:" + passVal);
+    }
+
+    public void CheckErrorMessage(String msg) {
+        assertThat("", errorEl.getHtmlText(), is(msg));
+        Utils.sleep(1000);
+    }
+}
--- src/main/java/org/fasttrackit/workshop/pagefactory/login/LoginPage.java
+++ src/main/java/org/fasttrackit/workshop/pagefactory/login/LoginPage.java
@@ -1,12 +1,15 @@
 package org.fasttrackit.workshop.pagefactory.login;
 
+import com.sdl.selenium.web.utils.Utils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class LoginPage {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginPage.class);
@@ -19,7 +22,12 @@ public class LoginPage {
 
     @FindBy(how = How.ID, using = "password")
     private WebElement passEl;
-
+/*
+    @FindBy(how = How.ID, using = "Logout")
+    private WebElement logoutButtonEl;
+*/
+    @FindBy(how = How.CLASS_NAME, using = "error-msg")
+    private WebElement errorEl;
 
     public void clickOnLoginButton() {
         loginButtonEl.click();
@@ -28,5 +36,21 @@ public class LoginPage {
     public void enterCredentials(String emailVal, String passVal) {
         emailEl.sendKeys(emailVal);
         passEl.sendKeys(passVal);
+    }
+/*
+    public void checkUserLogIn() {
+        boolean successfulLogin = false;
+        try {
+            successfulLogin = logoutButtonEl.isDisplayed();
+        } catch (Exception e) {
+
+        }
+        assertThat("Could not find logout button", successfulLogin, is(true));
+    }
+    */
+
+    public void CheckErrorMessage(String msg) {
+        assertThat(errorEl.getText(), is(msg));
+        Utils.sleep(4000);
     }
 }
